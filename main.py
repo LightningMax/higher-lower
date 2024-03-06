b90d203d8a3fefe84fabb8975b8679e9f86160d5
Max <maxreis507@gmail.com>
2024-03-06 19:24:36 +0100

    Add logo and art to main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+from art import *
 from game_data import data
 
 def choose_user():
@@ -11,7 +12,9 @@ second_user = choose_user()
 
 game_over = False
 
+print(logo)
 print(f"Compare A: {data[first_user]['name']}, a {data[first_user]['description']}, from {data[first_user]['country']}.")
+print(vs)
 print(f"Aganist B: {data[second_user]['name']}, a {data[second_user]['description']}, from {data[second_user]['country']}.")
 
 user_choice = input("Who has more followers? Type 'A' or 'B': ").upper()
